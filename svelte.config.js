b166e1a4d66850ac04eef60295b2fb4ba85953e2
Your Name <your.email@example.com>
2026-09-14 21:08:54 +0100

    fix: vitePreprocess + node22

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter()
   }
