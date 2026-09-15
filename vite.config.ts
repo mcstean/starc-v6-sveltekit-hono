211c7048da7ec14cf4d610046b7b84d154c27b88
Your Name <your.email@example.com>
2026-09-15 08:45:43 +0100

    fix: vite config for kit 2.16

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 export default defineConfig({
-  plugins: [svelte()],
-  server: { port: 5173 }
+  plugins: [sveltekit()]
 });
